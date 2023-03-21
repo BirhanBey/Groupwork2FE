6991e3100b1952c72c80aad6203811c63b3fe2d0
BirhanBey <114659714+BirhanBey@users.noreply.github.com>
2023-03-21 11:42:59 +0300

    add todo component added (#4)

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const AddTodo = ({ onAddTodo }) => {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddTodo && onAddTodo(title.trim());
+    setTitle('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add a new todo..."
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddTodo;
